79850ac775776ca75cd46ce4041a2aadec23d838
Shreshth Tuli <shreshthtuli@gmail.com>
2019-05-04 18:37:46 +0530

    Added sysbench results

--- linux/run.sh
+++
@@ -1,6 +0,0 @@
-sysbench --test=cpu --cpu-max-prime=5000 run > sysbench-primes-5k.txt
-sysbench --test=cpu --cpu-max-prime=10000 run > sysbench-primes-10k.txt
-sysbench --test=cpu --cpu-max-prime=15000 run > sysbench-primes-15k.txt
-sysbench --test=cpu --cpu-max-prime=20000 run > sysbench-primes-20k.txt
-sysbench --test=cpu --cpu-max-prime=25000 run > sysbench-primes-25k.txt
-sysbench --test=cpu --cpu-max-prime=30000 run > sysbench-primes-30k.txt
